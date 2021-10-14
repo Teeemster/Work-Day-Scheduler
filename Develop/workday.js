deab8518cedd6bfb1afdb0c132b2f8575a1341dd
Teeemster <jonathan_beach@ymail.com>
2021-10-13 19:10:35 -0700

    containers added

--- Develop/workday.js
+++ Develop/workday.js
@@ -1,5 +1,26 @@
 var timeBlock = $("#timeblock");
+var startingTime = 1
 
+
+//Main Div Dom Creation
 for (let i = 0; i < 24; i++) {
-$("#timeSection").append(timeBlock)    
+    $("#timeSection").append(` <div id="timeBlock" class="row d-flex justify-content-center">
+
+    <section class="hour col-1" id="timeSlotArea"></section>
+
+    <section class="row col-8 past"><input type="text" class="past border-0 col-12"></section>
+
+    <section class="d-flex saveBtn col-1 justify-content-center"><i style="font-size:24px" class="fa align-self-center"
+          id="lockButton">&#xf023;</i></section>
+
+          </div>
+        </section>`)
 };
+
+//Cascading Time
+//for (let i = 0; i < 24; i++) {
+ //   var timeSlots = startingTime++;
+  //  $("#timeSlotArea").append(startingTime)
+//}
+
+//Local Storage
