626fa65ce66b8923c1e5d43d75cc4dfcb32984ff
Teeemster <jonathan_beach@ymail.com>
2021-10-15 22:30:32 -0700

    local storage initial

--- Develop/workday.js
+++ Develop/workday.js
@@ -1,26 +1,38 @@
 var timeBlock = $("#timeblock");
-var startingTime = 1
+var startingTime = 1;
+var dayBlock = $("#currentDay");
+var currentTime = moment();
+var calendarInfo = $(".calendarInfo")
+var calendarInfoValue = calendarInfo.textContent
+dayBlock.textContent = currentTime.format("MMM DD, YYYY - HH:mm:ss a");
 
+
+//Current Time
+$("#currentDay").append(dayBlock.textContent)
 
 //Main Div Dom Creation
 for (let i = 0; i < 24; i++) {
-    $("#timeSection").append(` <div id="timeBlock" class="row d-flex justify-content-center">
+  $("#timeSection").append(` <div id="timeBlock" class="row d-flex justify-content-center">
 
-    <section class="hour col-1" id="timeSlotArea"></section>
+    <section class="hour col-1" id="timeSlotArea">${i + 1}:00 PST</section>
 
-    <section class="row col-8 past"><input type="text" class="past border-0 col-12"></section>
+    <section class="row col-8 past"><input type="text" class="past border-0 col-12 calendarInfo"></section>
 
-    <section class="d-flex saveBtn col-1 justify-content-center"><i style="font-size:24px" class="fa align-self-center"
-          id="lockButton">&#xf023;</i></section>
+    <section class="d-flex col-1 justify-content-center saveBtn"><button type="button" class="fa align-self-center"
+          id="lockButton">&#xf023;</button></section>
 
           </div>
-        </section>`)
+        </section>`);
 };
 
-//Cascading Time
-//for (let i = 0; i < 24; i++) {
- //   var timeSlots = startingTime++;
-  //  $("#timeSlotArea").append(startingTime)
-//}
-
 //Local Storage
+$("#lockButton").on("click", function () {
+    if (localStorage.getItem('Activities') == null) {
+      localStorage.setItem('Activities', '[]');
+    } 
+    localStorage.setItem('Activities', JSON.stringify(calendarInfoValue));
+    localStorage.getItem('Activities');
+}
+);
+
+
